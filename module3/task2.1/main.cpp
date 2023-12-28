e21943cfaceb012168c76e1ed774f737e9b6b253
IvanNaum <nuk04@mail.ru>
2023-12-28 14:18:17 +0300

    task3

--- module3/task2.1/main.cpp
+++ module3/task2.1/main.cpp
@@ -1,6 +1,131 @@
 #include <iostream>
+#include <cassert>
+#include <queue>
+
+
+struct IGraph {
+    virtual ~IGraph() {}
+
+    virtual void AddEdge(int from, int to) = 0;
+
+    virtual int VerticesCount() const = 0;
+
+    virtual std::vector<int> GetNextVertices(int vertex) const = 0;
+
+    virtual std::vector<int> GetPrevVertices(int vertex) const = 0;
+};
+
+
+struct ListGraph : public IGraph {
+    ListGraph(size_t size) : adjacency_list(size) {};
+
+    ListGraph(const IGraph &graph) : ListGraph(graph.VerticesCount()) {
+        for (int from = 0; from < graph.VerticesCount(); ++from) {
+            for (int to: graph.GetNextVertices(from)) {
+                AddEdge(from, to);
+            }
+        }
+    }
+
+    ~ListGraph() override = default;
+
+    void AddEdge(int from, int to) override {
+        assert(0 <= from && from < adjacency_list.size());
+        assert(0 <= to && to < adjacency_list.size());
+        adjacency_list.at(from).push_back(to);
+        adjacency_list.at(to).push_back(from);
+    }
+
+    int VerticesCount() const override {
+        return adjacency_list.size();
+    }
+
+
+    std::vector<int> GetNextVertices(int vertex) const override {
+        assert(0 <= vertex && vertex < adjacency_list.size());
+
+        return adjacency_list.at(vertex);
+    }
+
+    std::vector<int> GetPrevVertices(int vertex) const override {
+        assert(0 <= vertex && vertex < adjacency_list.size());
+
+        auto result = std::vector<int>();
+        for (size_t from = 0; from < adjacency_list.size(); ++from) {
+            for (int to: adjacency_list.at(from)) {
+                if (to == vertex) {
+                    result.push_back(from);
+                }
+            }
+        }
+
+        return result;
+    }
+
+private:
+    std::vector<std::vector<int>> adjacency_list;
+};
+
+
+int BFS(const IGraph &graph, int from, int to) {
+    std::vector<size_t> distances(graph.VerticesCount(), ~0ull);
+    std::vector<int> ways(graph.VerticesCount(), 0);
+    std::vector<bool> visited(graph.VerticesCount(), false);
+
+    visited[from] = true;
+
+    distances[from] = 0;
+    ways[from] = 1;
+
+    std::queue<int> queue;
+    queue.push(from);
+
+
+    while (!queue.empty()) {
+        int current = queue.front();
+        queue.pop();
+
+        for (int next: graph.GetNextVertices(current)) {
+            if (distances[current] + 1 == distances[next]) {
+                ways[next] += ways[current];
+            }
+            if (!visited[next]) {
+                if (distances[current] + 1 < distances[next]) {
+                    distances[next] = distances[current] + 1;
+                    ways[next] = ways[current];
+                }
+                visited[next] = true;
+                queue.push(next);
+            }
+        }
+    }
+    return ways[to];
+}
+
+void test() {
+    ListGraph graph(4);
+    graph.AddEdge(0, 1);
+    graph.AddEdge(0, 2);
+    graph.AddEdge(1, 3);
+    graph.AddEdge(2, 3);
+
+    std::cout << BFS(graph, 0, 3);
+}
 
 int main() {
-  std::cout << std::endl << "Success." << std::endl;
-  return 0;
+    size_t size;
+    std::cin >> size;
+    ListGraph graph(size);
+
+    std::cin >> size;
+    int from, to;
+    for (size_t i = 0; i < size; ++i) {
+        std::cin >> from >> to;
+        graph.AddEdge(from, to);
+    }
+
+    std::cin >> from >> to;
+    std::cout << BFS(graph, from, to) << '\n';
+
+    return 0;
 }
--- module3/task3.1/main.cpp
+++ module3/task3.1/main.cpp
@@ -1,6 +1,150 @@
 #include <iostream>
+#include <vector>
+#include <cassert>
+#include <queue>
+#include <set>
+#include <limits.h>
+
+
+struct IGraph {
+    virtual ~IGraph() {}
+
+    virtual void AddEdge(int from, int to, int weight) = 0;
+
+    virtual int VerticesCount() const = 0;
+
+    virtual int GetWeight(int from, int to) const = 0;
+
+    virtual std::vector<int> GetNextVertices(int vertex) const = 0;
+
+    virtual std::vector<int> GetPrevVertices(int vertex) const = 0;
+};
+
+
+struct MatrixGraph : public IGraph {
+    explicit MatrixGraph(size_t size);
+
+    explicit MatrixGraph(const IGraph &);
+
+    ~MatrixGraph() override;
+
+    void AddEdge(int from, int to, int weight) override;
+
+    int VerticesCount() const override;
+
+    std::vector<int> GetNextVertices(int vertex) const override;
+
+    std::vector<int> GetPrevVertices(int vertex) const override;
+
+    int GetWeight(int from, int to) const override;
+
+private:
+    std::vector<std::vector<int>> adjacency_matrix;
+};
+
+MatrixGraph::MatrixGraph(size_t size) : adjacency_matrix(size, std::vector<int>(size, 0)) {};
+
+MatrixGraph::MatrixGraph(const IGraph &graph) : MatrixGraph(graph.VerticesCount()) {
+    for (int from = 0; from < graph.VerticesCount(); ++from) {
+        for (int to: graph.GetNextVertices(from)) {
+            AddEdge(from, to, graph.GetWeight(from, to));
+        }
+    }
+}
+
+MatrixGraph::~MatrixGraph() = default;
+
+int MatrixGraph::GetWeight(int from, int to) const {
+    return adjacency_matrix.at(from).at(to);
+}
+
+void MatrixGraph::AddEdge(int from, int to, int weight) {
+    assert(0 <= from && from < adjacency_matrix.size());
+    assert(0 <= to && to < adjacency_matrix.size());
+
+    int exists_weight = GetWeight(from, to);
+    if (exists_weight == 0 || exists_weight > weight) {
+        adjacency_matrix.at(from).at(to) = weight;
+        adjacency_matrix.at(to).at(from) = weight;
+    }
+}
+
+int MatrixGraph::VerticesCount() const {
+    return adjacency_matrix.size();
+}
+
+std::vector<int> MatrixGraph::GetNextVertices(int vertex) const {
+    assert(0 <= vertex && vertex < adjacency_matrix.size());
+
+    std::vector<int> result;
+
+    for (int i = 0; i < adjacency_matrix.at(vertex).size(); ++i) {
+        if (adjacency_matrix.at(vertex).at(i) != 0) {
+            result.push_back(i);
+        }
+    }
+
+    return result;
+}
+
+std::vector<int> MatrixGraph::GetPrevVertices(int vertex) const {
+    assert(0 <= vertex && vertex < adjacency_matrix.size());
+
+    std::vector<int> result;
+
+    for (size_t i = 0; i < adjacency_matrix.size(); ++i) {
+        if (adjacency_matrix.at(i).at(vertex) != 0) {
+            result.push_back(i);
+        }
+    }
+
+    return result;
+}
+
+int get_shortest_way(const IGraph &graph, int from, int to) {
+    std::vector<bool> visited(graph.VerticesCount(), false);
+    std::vector<int> prev(graph.VerticesCount(), -1);
+    std::vector<int> dist(graph.VerticesCount(), INT_MAX);
+    dist[from] = 0;
+
+    auto cmp = [&dist](int v1, int v2) { return dist[v1] > dist[v2]; };
+    std::priority_queue<int, std::vector<int>, decltype(cmp)> que(cmp);
+
+    que.push(from);
+    while (!que.empty()) {
+        int vertex = que.top();
+        que.pop();
+
+        if (visited[vertex]) {
+            continue;
+        }
+        visited[vertex] = true;
+
+        for (int v: graph.GetNextVertices(vertex)) {
+            if (dist[v] > dist[vertex] + graph.GetWeight(vertex, v)) {
+                dist[v] = dist[vertex] + graph.GetWeight(vertex, v);
+                prev[v] = vertex;
+                que.push(v);
+            }
+        }
+    }
+    return dist[to];
+}
 
 int main() {
-  std::cout << std::endl << "Success." << std::endl;
-  return 0;
+    int N, M;
+    std::cin >> N >> M;
+
+    MatrixGraph graph(N);
+
+    int from, to, weight;
+    for (int i = 0; i < M; ++i) {
+        std::cin >> from >> to >> weight;
+        graph.AddEdge(from, to, weight);
+    }
+
+    std::cin >> from >> to;
+    std::cout << get_shortest_way(graph, from, to) << std::endl;
+
+    return 0;
 }
